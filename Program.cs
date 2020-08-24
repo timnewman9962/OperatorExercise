e0e553ff9bee8b2c4b7f96fe8c62701a6153f008
Tim Newman <timnewman9962@gmail.com>
2020-08-24 14:04:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        
+        static double AreaOfCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nPick a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nNow give me another number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine("\nHere is some math we can do:\n" +
+                            $"{a} + {b} is {a+b}\n" +
+                            $"{a} - {b} is {a-b}\n" +
+                            $"{a} * {b} is {a*b}\n" +
+                            $"{a} / {b} is {quotient} remainder {remainder}\n" 
+            );
+
+            Console.WriteLine("-------\nNow lets find the area of a circle.\nGive me the radius");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("The area is " + AreaOfCircle(radius));
+            
+        }
+    }
+}
